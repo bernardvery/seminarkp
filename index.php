219af6abddc6241e81c29007896a0cb758fbc86f
bernardvery <bernardvery@live.com>
2024-02-27 10:24:14 +0700

    file index

---
+++ index.php
@@ -0,0 +1,28 @@
+<style type="text/css">
+	table.tableizer-table {
+		font-size: 12px;
+		border: 1px solid #CCC; 
+		font-family: Arial, Helvetica, sans-serif;
+	} 
+	.tableizer-table td {
+		padding: 4px;
+		margin: 3px;
+		border: 1px solid #CCC;
+	}
+	.tableizer-table th {
+		background-color: #104E8B; 
+		color: #FFF;
+		font-weight: bold;
+	}
+</style>
+<table class="tableizer-table">
+<thead><tr class="tableizer-firstrow"><th>No</th><th>Mahasiswa</th><th>Judul</th><th>Penguji</th><th>Waktu Ujian</th><th>Keterangan</th></tr></thead><tbody>
+ <tr><td>1</td><td>ALFANANDO ILHAMSYAH  G.131.18.0031  S1 Sistem Informasi   6281328611825</td><td>COMPANY PROFIL UNTUK CV. YULISE WISATA BERBASIS WEBSITE MENGGUNAKAN FRAMEWORK BOOTSTRAP</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 80</td><td>Gelombang 1  Tanggal: 16 May 2023  Jam: 09:00:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>2</td><td>IBNU ABBAS  G.111.18.0030  S1 Sistem Informasi   62895329182669</td><td>SISTEM INFORMASI PEGAWAI DINAS ARSIP DAN PERPUSTAKAAN KOTA SEMARANG (DINASPUS) KOTA SEMARANG BERBASIS WEB DENGAN MENGGUNAKAN PHP DAN MYSQL</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 70</td><td>Gelombang 1  Tanggal: 30 May 2023  Jam: 09:00:00  Jenis Ujian: Offline  Ruang: M.3.3</td><td>&nbsp;</td></tr>
+ <tr><td>3</td><td>PUSPITA RAHAYU  G.131.19.0072  S1 Sistem Informasi   6282139317989</td><td>Penggunaan websocket pada pengiriman informasi antar client dan server</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 90</td><td>Gelombang 3  Tanggal: 12 July 2023  Jam: 09:00:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>4</td><td>MARIANITA CHRISRTINE  G.231.19.0174  S1 Teknik Informatika   6281225542125</td><td>Aplikasi Manajemen Inventaris Kendaraan menggunakan Framework Laravel pada PT. Jawasurya Kencanaindah.</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 85</td><td>Gelombang 3  Tanggal: 24 July 2023  Jam: 09:00:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>5</td><td>SYIFA AZQIA MOULINAR  G.131.19.0074  S1 Sistem Informasi   6281226949802</td><td>Optimasi Website Pemasangan Produk Iconnet Menggunakan Teknik Search Engine Optimization (SEO) di PT. Taman Media Digital</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 85</td><td>Gelombang 1  Tanggal: 24 July 2023  Jam: 09:30:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>6</td><td>FITRI RESTU ARIANI  G.131.19.0065  S1 Sistem Informasi   6289530861775</td><td>SISTEM INFORMASI ABSENSI GURU DAN SISWA BERBASIS WEB DI SD NEGERI 3 KARAS</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 75</td><td>Gelombang 3  Tanggal: 24 July 2023  Jam: 10:30:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>7</td><td>ZULVIRA SALSABILA PRASETYA  G.131.18.0066  S1 Sistem Informasi   6287736842989</td><td>Sistem Informasi Keuangan Berbasis Web pada CV. Terakota Solutama</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 75</td><td>Gelombang 3  Tanggal: 02 August 2023  Jam: 09:00:00  Jenis Ujian: Offline  Ruang:</td><td>&nbsp;</td></tr>
+ <tr><td>8</td><td>DEWA KUNWELDAN  G.231.18.0135  S1 Teknik Informatika   6289644757629</td><td>Aplikasi Pendataan Karyawan Dan Peserta Magang Di CV Techarea Indonesia Jaya</td><td>Penguji 1: B. VERY CHRISTIOKO, S.Kom., M.Kom.  Hasil Ujian:  Status Lulus : Lulus Nilai Penguji 1: 80</td><td>Gelombang 4  Tanggal: 08 August 2023  Jam: 10:00:00  Jenis Ujian: Offline  Ruang:</td><td></td></tr>
+</tbody></table>
